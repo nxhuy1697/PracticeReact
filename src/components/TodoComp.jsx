174962fd84fdd846e7f970d05c472f00a6dcac39
nxhuy1697 <nxhuy1697@gmail.com>
2024-03-07 09:44:54 +0700

    update

--- src/components/TodoComp.jsx
+++ src/components/TodoComp.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useRef } from "react";
 //userReducer thực hành:
 //b1: Init State (xác định giá trị khởi tạo)
 const initState = {
@@ -10,48 +10,86 @@ const SET_JOB = "set_job";
 const ADD_JOB = "add_job";
 const DELETE_JOB = "delete_job";
 
-const setJob = payload => {
-    return {
-        type : SET_JOB,
-        payload
-    }
-}
-
-
+const setJob = (payload) => {
+  return {
+    type: SET_JOB,
+    payload,
+  };
+};
+const addJob = (payload) => {
+  return {
+    type: ADD_JOB,
+    payload,
+  };
+};
+const deleteJob = (payload) => {
+  return {
+    type: DELETE_JOB,
+    payload,
+  };
+};
 //b3: tạo Reducer
 const reducer = (state, action) => {
-
-    switch (action.type){
-        case SET_JOB :
-            return {
-                ...state,
-                job: action.payload
-            }
-            default:
-                throw new Error('Invalid')
-    }
-
+  let newState;
+  switch (action.type) {
+    case SET_JOB:
+      newState = {
+        ...state,
+        job: action.payload,
+      };
+      break;
+    case ADD_JOB:
+      newState = {
+        ...state,
+        jobs: [...state.jobs, action.payload],
+      };
+      break;
+    case DELETE_JOB:
+      const newJobs = [...state.jobs];
+      newJobs.splice(action.payload, 1);
+      newState = {
+        ...state,
+        jobs: newJobs
+      };
+      break;
+    default:
+      throw new Error("Invalid");
+  }
+  console.log("new state: ", newState);
+  return newState;
 };
 //b4: dispatch (đi kích hoạt 1 action)
 
 export default function TodoComp() {
   const [state, dispatch] = useReducer(reducer, initState);
   const { job, jobs } = state;
+  const inputRef = useRef();
+  const handleSubmit = () => {
+    dispatch(addJob(job));
+    dispatch(setJob(""));
+
+    inputRef.current.focus();
+  };
+
   return (
     <div style={{ padding: 30 }}>
       <h1>What we're gonna do today?</h1>
       <input
+        ref={inputRef}
         placeholder="Enter your works need to be done here"
         style={{ width: 500 }}
         value={job}
-        onChange={e => {
-            dispatch(setJob(e.target.value))
+        onChange={(e) => {
+          dispatch(setJob(e.target.value));
         }}
       />
-      <button>Add</button>
+      <button onClick={handleSubmit}>Add</button>
       <ul>
         {jobs.map((job, index) => (
-          <li key={index}> {job} &times; </li>
+          <li key={index}>
+            {" "}
+            {job} <span onClick={() => dispatch(deleteJob(index))}>&times;</span>{" "}
+          </li>
         ))}
       </ul>
     </div>
